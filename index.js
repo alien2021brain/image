f1f2a4f43ad62645843b1e8951e6512d1911ed30
alien2021brain <sachinpandey262523@gmail.com>
2023-06-20 09:44:19 +0530

    20

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("js is working")
+const right = document.querySelector('.right');
+const slider = document.querySelector('.slider');
+let current = 1;
+
+right.addEventListener('click', () => {
+  slider.style.transform = `translateX(-${current * 800}px)`;
+  current++;
+});
